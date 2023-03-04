7011a7f761e0e6f00c4c9114672826449d83bbf6
houdunren <2300071698@qq.com>
2023-03-04 22:05:00 +0800

    修复window系统拖拽放大的问题

--- src/main/drag.ts
+++ src/main/drag.ts
@@ -1,7 +1,9 @@
 import { BrowserWindow, ipcMain, IpcMainInvokeEvent } from 'electron'
-ipcMain.handle('drag', (event: IpcMainInvokeEvent, opt: { x: number; y: number }) => {
-  const win = BrowserWindow.fromWebContents(event.sender)!
-
-  const [x, y] = win.getPosition()
-  win.setPosition(x + opt.x, y + opt.y)
-})
+ipcMain.handle(
+  'drag',
+  (event: IpcMainInvokeEvent, opt: { x: number; y: number; width: number; height: number }) => {
+    const win = BrowserWindow.fromWebContents(event.sender)!
+    const { x, y } = win.getBounds()
+    win.setBounds({ x: x + opt.x, y: y + opt.y, width: opt.width, height: opt.height })
+  }
+)
--- src/renderer/src/composables/useDrag.ts
+++ src/renderer/src/composables/useDrag.ts
@@ -1,6 +1,11 @@
 class Drag {
   private body?: HTMLBodyElement
-  constructor(private resPageX = 0, private resPageY = 0) {}
+  constructor(
+    private resPageX = 0,
+    private resPageY = 0,
+    private innerWidth = 0,
+    private innerHeight = 0
+  ) {}
   public run() {
     window.addEventListener('DOMContentLoaded', () => {
       this.body = document.querySelector('body')!
@@ -10,6 +15,8 @@ class Drag {
   private down(e: MouseEvent) {
     this.resPageX = e.pageX
     this.resPageY = e.pageY
+    this.innerWidth = window.innerWidth
+    this.innerHeight = window.innerHeight
     const mouseEvent = this.move.bind(this)
     this.body!.addEventListener('mousemove', mouseEvent)
     this.body!.addEventListener('mouseup', () =>
@@ -19,7 +26,7 @@ class Drag {
   private move(e: MouseEvent) {
     const x = e.pageX - this.resPageX
     const y = e.pageY - this.resPageY
-    window.api.drag({ x, y })
+    window.api.drag({ x, y, width: this.innerWidth, height: this.innerHeight })
   }
 }
 
