6614750ee88c9fd5f66b2723f6d3777522eb664f
houdunren <2300071698@qq.com>
2023-06-14 03:07:28 +0800

    更新检测

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -4,22 +4,24 @@ import Camera from './views/Camera.vue'
 import Setting from './views/Setting.vue'
 import Secret from './views/Secret.vue'
 import Updater from './views/Updater.vue'
+// import useUpdater from './composables/useUpdater'
 const { config } = useConfigStore()
+config.page = 'updater'
+// const { checkUpdate } = useUpdater()
+// config.page = 'updater'
+// checkUpdate()
+
 //右键菜单
 // const contextMenu = () => window.api.contextMenu()
 </script>
 
 <template>
   <Suspense>
-    <main class="relative1">
-      <Updater />
-      <section>
-        <Secret v-if="!config.token" />
-        <div v-else>
-          <Camera v-if="config.page == 'camera'" />
-          <Setting v-if="config.page == 'setting'" />
-        </div>
-      </section>
+    <main class="relative">
+      <Updater v-if="config.page == 'updater'" />
+      <Secret v-if="config.page == 'secret'" />
+      <Camera v-if="config.page == 'camera'" />
+      <Setting v-if="config.page == 'setting'" />
     </main>
   </Suspense>
 </template>
--- src/renderer/src/views/Updater.vue
+++ src/renderer/src/views/Updater.vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
 import { http } from '@renderer/axios'
 import { useConfigStore } from '@renderer/stores/useConfigStore'
+import packageJson from '../../../../package.json'
+import { ref } from 'vue'
+
 const { config } = useConfigStore()
-try {
-  await http.request({
-    url: `/token/check`,
+const checkResult = ref(9)
+http
+  .request({
+    url: `/soft/checkUpdate`,
     method: 'POST',
     data: {
-      token: config.token,
-      soft: 'camera'
+      version: packageJson.version,
+      name: 'camera'
     }
   })
-} catch (error) {
-  config.token = ''
-}
+  .then((res) => {
+    setTimeout(() => {
+      checkResult.value = res.data
+      if (res.data == 0) {
+        config.page = 'camera'
+      }
+    }, 1000)
+  })
+  .catch(() => {
+    config.token = ''
+  })
 </script>
 
 <template>
-  <main class=""></main>
+  <main
+    id="updater"
+    class="text-xl font-sans font-light text-slate-800 flex flex-col justify-center items-center w-screen h-screen z-50"
+  >
+    <section v-if="checkResult == 9" class="bg-violet-700 text-white">更新检测中...</section>
+    <section v-if="checkResult == 1" class="bg-orange-300 duration-500">
+      <div>有新版本了</div>
+      <div>请访问 <span class="font-bold">hdcms.com</span> 网站下载</div>
+    </section>
+  </main>
 </template>
+
+<style lang="scss" scoped>
+section {
+  @apply flex justify-center items-center flex-col w-screen h-screen;
+}
+</style>
