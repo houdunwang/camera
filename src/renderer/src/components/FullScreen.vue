da6643831e1bd1a0c55be81aa57554a7a705c085
houdunren <2300071698@qq.com>
2023-06-26 03:56:17 +0800

    fix:全屏使用DOM控制

--- src/renderer/src/components/FullScreen.vue
+++ src/renderer/src/components/FullScreen.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import { FullScreen } from '@icon-park/vue-next'
-import useCamera from '@renderer/composables/useCamera'
-const { toggleFullscreen } = useCamera()
+// import useCamera from '@renderer/composables/useCamera'
+import { ref } from 'vue'
+// const { toggleFullscreen } = useCamera()
+
+const isFullScreen = ref<boolean>(false)
+const toggleFullScreen = () => {
+  isFullScreen.value ? document.exitFullscreen() : document.documentElement.requestFullscreen()
+  isFullScreen.value = !isFullScreen.value
+}
+
+document.addEventListener('fullscreenchange', (e) => {
+  isFullScreen.value = Boolean(document.fullscreenElement)
+})
 </script>
 
 <template>
@@ -11,7 +22,7 @@ const { toggleFullscreen } = useCamera()
     fill="#ffffff"
     class="icon"
     :strokeWidth="5"
-    @click="toggleFullscreen"
+    @click="toggleFullScreen"
   />
 </template>
 
