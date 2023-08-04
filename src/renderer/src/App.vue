d6721c26bd3ca63a18804008e98a9e04adc0aa15
houdunren <2300071698@qq.com>
2023-08-05 03:59:09 +0800

    1.0.53

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -2,14 +2,14 @@
 import Footer from '@renderer/components/Footer.vue'
 // import Secret from '@renderer/hd/Secret.vue'
 import Updater from '@renderer/hd/Updater.vue'
-import useSoft from '@renderer/hd/useSoft'
+// import useSoft from '@renderer/hd/useSoft'
 import { useConfigStore } from '@renderer/stores/useConfigStore'
 import Camera from './views/Camera.vue'
 import Setting from './views/Setting.vue'
 const { config } = useConfigStore()
 config.page = 'camera'
-const { init } = useSoft()
-init()
+// const { init } = useSoft()
+// init()
 </script>
 
 <template>
